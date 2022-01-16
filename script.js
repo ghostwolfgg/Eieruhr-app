729a35f0fa7568e4a3082c8932df673b0f61d241
Miguel <82134840+ghostwolfgg@users.noreply.github.com>
2022-01-16 13:16:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let alarm = new Audio('alarm.mp3');
+let timerStarted = false;
+
+
+function startTimer() {
+    if (!timerStarted) {
+        let startTime = new Date().getTime();
+        let fiveMinutes = 1000 * 60 * 5;
+        let endTime = startTime + fiveMinutes;
+
+
+        timerStarted = true;
+        let interval = setInterval(()=> {
+            let timeLeft = endTime - new Date().getTime();
+
+
+            if (timeLeft > 0) {
+                let minutes = timeLeft / (1000 * 60);
+                minutes = Math.floor(minutes);
+                
+                let seconds = (timeLeft / 1000) % 60;
+                seconds = Math.round(seconds);
+
+                seconds = ('0' + seconds).slice(-2);
+
+                let text = '0' + minutes + ' : ' + seconds;
+                timer.innerHTML = text;
+            } else {
+                alarm.play();
+                timer.innerHTML = '00 : 00';
+                setTimeout(()=>{
+                    alarm.pause();
+                    timerStarted = false;
+                    clearInterval(interval);
+                }, 7000)
+            }
+        }, 1000);
+    }
+}
